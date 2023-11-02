855c86f5dc84f063e24a0ce3046aea8b1d460e12
Rishabh Singh <rishabhsingh50689@gmail.com>
2023-11-02 09:39:03 +0530

    Done

---
+++ packages/evershop/src/modules/cms/api/folderCreate/createFolder.js
@@ -0,0 +1,34 @@
+const { join, basename } = require('path');
+const { existsSync, mkdirSync } = require('fs');
+const { CONSTANTS } = require('@evershop/evershop/src/lib/helpers');
+const {
+  INVALID_PAYLOAD,
+  OK
+} = require('@evershop/evershop/src/lib/util/httpStatus');
+const { getConfig } = require('@evershop/evershop/src/lib/util/getConfig');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (request, response, delegate, next) => {
+  if (getConfig('file_storage') !== 'local') {
+    next();
+  } else {
+    const { path } = request.body || '';
+    const targetPath = join(CONSTANTS.MEDIAPATH, path);
+    if (existsSync(targetPath)) {
+      response.status(INVALID_PAYLOAD).json({
+        error: {
+          status: INVALID_PAYLOAD,
+          message: 'Folder already existed'
+        }
+      });
+    } else {
+      mkdirSync(targetPath, { recursive: true });
+      response.status(OK).json({
+        data: {
+          path,
+          name: basename(targetPath)
+        }
+      });
+    }
+  }
+};
